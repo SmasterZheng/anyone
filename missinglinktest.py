492edd2dc4176a6557f8768d34b76f2afdd65c37
SmasterZheng <xiaozhangzxz@163.com>
2019-01-14 23:54:11 +0800

    missinglink-test

--- missinglinktest.py
+++ missinglinktest.py
@@ -12,18 +12,18 @@ with missinglink_project.create_experiment(
 # replace
 # for step in range(MAX_STEPS):
 # with 
-        for step in experiment.loop(max_iterations=MAX_STEPS):
+    for step in experiment.loop(max_iterations=MAX_STEPS):
         with experiment.train():
             _, loss_value = session.run([train_op, loss], feed_dict=feed_dict)
-        if (step + 1) % 500 == 0 or (step + 1) == MAX_STEPS:
-            with experiment.validation():
-            do_eval(session, eval_correct, images_placeholder,
+            if (step + 1) % 500 == 0 or (step + 1) == MAX_STEPS:
+                with experiment.validation():
+                do_eval(session, eval_correct, images_placeholder,
                     labels_placeholder, data_sets.validation)
-        total_test_iterations = data_set.num_examples
+                    total_test_iterations = data_set.num_examples
 
-            with experiment.test (
-                total_test_iterations,
-                expected=labels_placeholder,
-                predicted=logits 
-                ):
-                sess.run([train_op, loss], feed_dict=feed_dict)
+                with experiment.test (
+                    total_test_iterations,
+                    expected=labels_placeholder,
+                    predicted=logits 
+                    ):
+                    sess.run([train_op, loss], feed_dict=feed_dict)
