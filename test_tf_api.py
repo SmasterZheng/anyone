3bfcd301fa83734fa647d3b874a190517b8caf59
SmasterZheng <xiaozhangzxz@163.com>
2019-01-18 23:55:45 +0800

    test_tf_api

---
+++ test_tf_api.py
@@ -0,0 +1,9 @@
+import unittest
+import numpy as np
+import time
+
+class TFserving(object):
+	"""docstring for ClassName"""
+	def __init__(self):
+		self.image='./test.jpg'
+		self.time=time.localtime
