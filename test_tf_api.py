67e9ecbf40ec1e7fcd587bcb4ead136b263451ab
SmasterZheng <xiaozhangzxz@163.com>
2019-01-21 23:00:55 +0800

    scond

--- test_tf_api.py
+++ test_tf_api.py
@@ -29,28 +29,28 @@ class TestTfServing(unittest.TestCase):
 
     def test_lane_post(self):
         print("\n in func: {}".format(inspect.stack()[0][3]))
-        img_buf_once = cv2.imread(self.test_img)
-        img_buf_once = cv2.cvtColor(img_buf_once, cv2.COLOR_RGB2GRAY)
-        resized_image = cv2.resize(img_buf_once, (512, 288))
-        image_buf = np.reshape(resized_image, (1, 288, 512, 1))
-        # with open(self.test_img,"rb") as imageFile:
-        #     image_buf=base64.b64encode(imageFile.read())
+        # img_buf_once = cv2.imread(self.test_img)
+        # img_buf_once = cv2.cvtColor(img_buf_once, cv2.COLOR_RGB2GRAY)
+        # resized_image = cv2.resize(img_buf_once, (512, 288))
+        # image_buf = np.reshape(resized_image, (1, 288, 512, 1))
+        with open(self.test_img,"rb") as imageFile:
+            image_buf=base64.b64encode(imageFile.read())
 
-        for _ in range(self.test_times):
-            start_time = time.time()
-            post_data = json.dumps({"inputs": {"Placeholder:0": image_buf.tolist()}})
-            # post_data = json.dumps({"inputs": {"image": image_buf}})
-            # print(post_data)
-            print("pack-{}, use time: {} ms".format(image_buf.shape,
-                                                    (time.time() - start_time) * 1000))
-            res = requests.post(
-                "{}/v1/models/lane_v1:predict".format(TF_ENDPOINT),
-                data=post_data
-                # verify=False, headers=headers
-            )
-            # print(res.text)
-            print("lane\n", dict(res.json()).keys())
-            print("use time: {} ms".format((time.time() - start_time) * 1000))
+	        for _ in range(self.test_times):
+	            start_time = time.time()
+	            # post_data = json.dumps({"inputs": {"Placeholder:0": image_buf.tolist()}})
+	            post_data = json.dumps({"inputs": {"Placeholder:0": str(image_buf)}})
+	            # print(post_data)
+	            print("pack-{}, use time: {} ms".format(image_buf.shape,
+	                                                    (time.time() - start_time) * 1000))
+	            res = requests.post(
+	                "{}/v1/models/lane_v1:predict".format(TF_ENDPOINT),
+	                data=post_data
+	                # verify=False, headers=headers
+	            )
+	            # print(res.text)
+	            print("lane\n", dict(res.json()).keys())
+	            print("use time: {} ms".format((time.time() - start_time) * 1000))
 
 if __name__ == '__main__':
     test_tf_serving=test_tf_restapi.TestTfServing()
